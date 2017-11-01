be6c1bf266451627b10ccc8633c90039ae5a56cb
Dusan Mijatovic (Linux) <d.mijatovic@dv4all.nl>
2017-11-01 08:53:31 +0100

    router 4 added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import React, { Component } from 'react';
 
-//import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import { AppHead, AppFooter } from './layout';
 
@@ -31,15 +31,24 @@ import './App.scss';
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <AppHead /> 
-        <section className="app-body">
-          <p className="app-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </section>
-        <AppFooter />
-      </div>
+      <BrowserRouter>
+
+        <div className="app">
+          <AppHead />      
+
+          <section className="app-body">
+            <Switch>
+              <Route exact path="/" component={HomePage}/>
+              <Route path="/home" component={HomePage}/>
+              <Route path="/user" component={UserPage}/>                  
+            </Switch>
+          </section>
+          
+          <AppFooter />
+
+        </div>
+
+      </BrowserRouter>
     );
   }
 }
